a98676a97a7c93e521e54e4aac68c1727c7d935d
Tsvetomir Shkondachki <cvetomir.shk@gmail.com>
2026-01-03 13:23:26 +0200

    Fixed unsued var bug

--- client/src/components/ErrorBoundary.tsx
+++ client/src/components/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ErrorInfo, ReactNode } from 'react';
+import { Component, ErrorInfo, ReactNode } from 'react';
 import styles from './ErrorBoundary.module.scss';
 
 interface Props {
